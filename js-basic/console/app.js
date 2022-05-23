f736a11046af5946dad2fc626aa47d6b38d608d6
volkan <volkancaliskan14@gmail.com>
2022-05-24 01:43:51 +0300

    js basict coursers

---
+++ js-basic/console/app.js
@@ -0,0 +1,41 @@
+// console.log("Hello World");
+// let name = "John";
+// console.log(name);
+// let age = 101;
+// console.log(age);
+// console.log(`${name} is ${age} years old`);
+
+// console.log(typeof "John");
+// console.log(typeof(age))
+
+// console.warn("This is a warning");
+// console.error("This is an error");
+// console.info("This is some info");
+// console.clear();
+
+/*
+Switch Hesap Maknisesi
+*/
+
+// sayi1 = prompt("Sayı giriniz");
+// sayi2 = prompt("Sayı giriniz");
+// islem = prompt("İşlem giriniz");
+
+// switch (islem) {
+//     case "+":
+//         console.log(sayi1 + sayi2);
+//         break;
+//     case "-":
+//         console.log(sayi1 - sayi2);
+//         break;
+//     case "*":
+//         console.log(sayi1 * sayi2);
+//         break;
+//     case "/":
+//         console.log(sayi1 / sayi2);
+//         break;
+//     default:
+//         console.log("Geçersiz işlem");
+//         break;
+// }
+
