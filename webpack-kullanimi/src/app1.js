5ff22ce88d988f036ed9265546b9443b116e10ec
volkan <volkancaliskan14@gmail.com>
2022-05-28 05:57:27 +0300

    webpack

---
+++ webpack-kullanimi/src/app1.js
@@ -0,0 +1,7 @@
+export class Person {
+    static Test(){
+        console.log("Person Test");
+    }
+}
+const text = "Hello World";
+document.getElementById("person").innerHTML = text;
---
+++ webpack-kullanimi/src/app2.js
@@ -0,0 +1,7 @@
+export class Employee {
+    static Test() {
+        console.log("Employee Test");
+    }
+}
+const text = "Hello World";
+document.getElementById("employee").innerHTML = text;
